95d6c31879966c950c190566d8e005a6b1d7f044
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2020-03-01 16:26:20 +0000

    Creates home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import App from "./App";
+import { createMemoryHistory } from 'history';
+import { render, wait, waitForDomChange } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from 'react-router-dom';
 
-test('this test should pass', () => {
-  expect(true).toBeTruthy();
+describe('Main landing page', () => {
+  it('this test should pass', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('has a top navigation bar', () => {
+    const { queryByTestId } = render(<App/>, {wrapper: MemoryRouter});
+    expect(queryByTestId('navigation-bar')).toBeInTheDocument();
+  })
+
+  it('has a home page title and tag line', ()=>{
+    const { getByText } = render(<App />, {wrapper: MemoryRouter});
+    expect(getByText('glūck')).toBeInTheDocument();
+    expect(getByText('gifting made happy')).toBeInTheDocument();
+  })
+
+  it('has a button find out more to go to About page',  () => {
+    const { getByText } = render(<App />, {wrapper: MemoryRouter});
+    const history = createMemoryHistory();
+    userEvent.click(getByText('Find out more'));
+    wait( () => {
+      expect(getByText('About Page')).toBeInTheDocument();
+      expect(history.location.pathname).toEqual('/about');
+    })
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path='/about'>
+                    <div>About Page</div>
+                </Route>
+                <Route path='/'>
+                    <div>glūck</div>
+                    <div data-testid={'navigation-bar'}>gifting made happy</div>
+                    <Link to={'/about'}>
+                        <button>Find out more</button>
+                    </Link>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
-export default withRouter(App);
+export default App;
