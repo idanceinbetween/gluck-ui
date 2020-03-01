e6d89fe16ef593da79d524caf5dda558cafdf2a9
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2020-03-01 17:26:31 +0000

    :pencil2: Runs prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/about'>
-                    <div>About Page</div>
-                </Route>
-                <Route path='/'>
-                    <div>glūck</div>
-                    <div data-testid={'navigation-bar'}>gifting made happy</div>
-                    <Link to={'/about'}>
-                        <button>Find out more</button>
-                    </Link>
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <div>About Page</div>
+        </Route>
+        <Route path="/">
+          <div>glūck</div>
+          <div data-testid={"navigation-bar"}>gifting made happy</div>
+          <Link to={"/about"}>
+            <button>Find out more</button>
+          </Link>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
