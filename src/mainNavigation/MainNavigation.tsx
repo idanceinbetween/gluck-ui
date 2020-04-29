24f2f56f17f76cab1d184335476d2d5e1d375ed0
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2020-04-29 21:20:11 +0100

    :lipstick: Adds stylesheet to Main Navigation bar
    Basic styles and font

--- src/mainNavigation/MainNavigation.tsx
+++ src/mainNavigation/MainNavigation.tsx
@@ -1,19 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const logo = require("./assets/logo.png");
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './mainNavigation.module.scss';
+import Logo from './assets/logo.png';
 
 export function MainNavigation() {
   return (
-    <nav role={"navigation"}>
-      <img alt={"logo"} src={String(logo)} />
-      <div>glūck</div>
-      <div>All Gifts</div>
-      <div>
-        <Link to={"/about"}>About</Link>
+    <nav role={'navigation'} className={styles.mainNav}>
+      <div className={styles.mainNavLogoContainer}>
+        <div className={styles.mainNavItemsContent}>
+          <img alt={'logo'} src={Logo} />
+        </div>
+        <div className={styles.mainNavItemsContent}>glūck</div>
       </div>
-      <div>Add A Gift</div>
-      <div>My Exchanges</div>
-      <div>My Account</div>
+      <div className={styles.mainNavItemsContainer}>
+        <ul className={styles.mainNavItemsContent}>
+          <li>All Gifts</li>
+          <li>
+            <Link to={'/about'}>About</Link>
+          </li>
+          <li>Add A Gift</li>
+          <li>My Exchanges</li>
+          <li>My Account</li>
+        </ul>
+      </div>
     </nav>
   );
 }
